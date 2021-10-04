cfda4b08b9e9de8b4d3f2992c25859065b46a656
mohsen <mohsen.minavand1384@gmail.com>
2021-10-04 20:42:51 +0330

    add try except for if there is any error in get data function , run the get_data again , this just a test .

--- video-call.py
+++ video-call.py
@@ -118,6 +118,7 @@ class procces :
             self.server_socket.send(struct.pack("L" , len(data)) + data)
         except : print("oh no yamete kodasai")
 
+                                                            #TODO delete this try and except they are so slow bro .  
 
     def send_data_h(self , frame_data) : 
         data = pickle.dumps(frame_data)
@@ -136,25 +137,29 @@ class procces :
 
         data = b""
         payload_size = struct.calcsize("L")
-        while True :
-            while len(data) < payload_size :
-                data += socket_connection.recv(4096)
-            
-            packed_msg_size = data[:payload_size]
-            data = data[payload_size:]
-            msg_size = struct.unpack("L" , packed_msg_size)[0]
-            n = 0
-            while len(data) < msg_size : 
-                data += socket_connection.recv(4096)
-            frame_data = data[:msg_size]
-            data = data[msg_size:]
-            
-            frame = pickle.loads(frame_data)
-            if frame :
-                ui.webcam_show_1(frame)
-            else : 
-                print("shit we can't get the data ") #TODO DELETE THIS ELSE CONDITION AFTER TEST .
-                print(frame)
+        try : 
+            while True :
+                while len(data) < payload_size :
+                    data += socket_connection.recv(4096)
+                
+                packed_msg_size = data[:payload_size]
+                data = data[payload_size:]
+                msg_size = struct.unpack("L" , packed_msg_size)[0]
+                n = 0
+                while len(data) < msg_size : 
+                    data += socket_connection.recv(4096)
+                frame_data = data[:msg_size]
+                data = data[msg_size:]
+                
+                frame = pickle.loads(frame_data)
+                if frame :
+                    ui.webcam_show_1(frame)
+                else : 
+                    print("shit we can't get the data ") #TODO DELETE THIS ELSE CONDITION AFTER TEST .
+                    print(frame)
+
+        except : 
+            self.get_data(type)
 
 
 
